fc06d670b121ce0d6b6c09d114693d38bbbbc390
LUIS I ROSALES JR <luis.rosalesjr01@gmail.com>
2022-07-13 22:17:39 -0600

    added questions 6 and 7 as well as point counter

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 'use strict';
+let userPoints = 0;
+let userWelcome = prompt('Let\'s play a game each question right is one point')
 
 let siteGuest = prompt('What is your name?');
 alert(`Hello ${siteGuest} welcome to my page!`);
@@ -6,6 +8,7 @@ alert(`Hello ${siteGuest} welcome to my page!`);
 let userGuess = prompt('Yes or No? I have black hair.').toLowerCase();
 if(userGuess === 'y' || userGuess === 'yes'){
   alert('YES I DO!');
+  userPoints++;
 }else if(userGuess === 'n' || userGuess === 'no'){
   alert('SORRY YES I DO!');
 }else{
@@ -15,6 +18,7 @@ if(userGuess === 'y' || userGuess === 'yes'){
 let userGuess2 = prompt('Yes or No? I have brown eyes.').toLowerCase();
 if(userGuess2 === 'y' || userGuess2 === 'yes'){
   alert('YES THEY ARE!');
+userPoints++;
 }else if(userGuess2 === 'n' || userGuess2 === 'no'){
   alert('SORRY MY EYES ARE BROWN!');
 }else{
@@ -24,6 +28,7 @@ if(userGuess2 === 'y' || userGuess2 === 'yes'){
 let userGuess3 = prompt('Yes or No? I am serving in the US Army.').toLowerCase();
 if(userGuess3 === 'y' || userGuess3 === 'yes'){
   alert('YES I AM!');
+userPoints++;
 }else if(userGuess3 === 'n' || userGuess3 === 'no'){
   alert('SORRY YES I AM!');
 }else{
@@ -33,6 +38,7 @@ if(userGuess3 === 'y' || userGuess3 === 'yes'){
 let userGuess4 = prompt('Yes or No? I am a mechanic.').toLowerCase();
 if(userGuess4 === 'y' || userGuess4 === 'yes'){
   alert('SURE AM!');
+userPoints++;
 }else if(userGuess4 === 'n' || userGuess4 === 'no'){
   alert('SORRY! I AM!');
 }else{
@@ -42,8 +48,51 @@ if(userGuess4 === 'y' || userGuess4 === 'yes'){
 let userGuess5 = prompt('Yes or No? Pasta is my favorite food.').toLowerCase();
 if(userGuess5 === 'y' || userGuess5 === 'yes'){
   alert(`YES IT IS ${siteGuest}!`);
+userPoints++;
 }else if(userGuess5 === 'n' || userGuess5 === 'no'){
   alert(`SORRY YES IT IS ${siteGuest}!`);
 }else{
   alert(`OK GAME OVER ${siteGuest}!`);
 }
+
+let myGuess = 8;
+let numGuess = 4;
+let theAnswer = false;
+
+while(numGuess && !theAnswer){
+let userGuess = prompt('Guess a number 1-10');
+  numGuess--; 
+  if(parseInt(userGuess)=== myGuess){
+    theAnswer = true;
+    alert('you got it!');
+userPoints++;
+  }if(userGuess > myGuess){
+      alert(`you are to high, remaining guesses is ${numGuess}`)
+  }if(userGuess < myGuess){
+    alert(`you are too low, remaining guesses is ${numGuess}`)
+  }if(numGuess === 0){
+    alert(`sorry ${numGuess} guesses it was 8`);}
+    }
+
+    let myFav = ['pasta', 'burritos', 'tacos'];
+    let numTime = 6;
+    let answer = false;
+    
+    while(numTime && !answer){
+    let userGuess = prompt('what is my favorite food?');
+    numTime--; 
+      for(let i = 0; i < myFav.length; i++){
+        if(userGuess === myFav[i]){
+          answer = true;
+          alert('your right I love pasta tacos and burritos');
+        userPoints++;
+        break;}
+      if(userGuess !== myFav){
+        console.log('try again');
+      }if(numTime === 0){ 
+        alert('sorry no more guesses its pasta, tacos and burritos');
+      break;}}
+    }    
+
+
+  alert('you have ' + userPoints + ' points.');
